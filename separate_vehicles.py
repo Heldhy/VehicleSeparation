0a8ca9fad4b39bd721d56fb86b3ac0791ef4ad99
Danae Marmai <danae.marmai@epita.fr>
2020-09-03 15:53:43 +0200

    requirement + clipping fixes

--- separate_vehicles.py
+++ separate_vehicles.py
@@ -27,10 +27,10 @@ def noise_removal(img):
 
 def separate_vehicles_by_size(denoised_img, separation_size):
     nb_components, output, stats, centroids = cv.connectedComponentsWithStats(denoised_img, connectivity=8)
-    #dropping background
+    # dropping background
     sizes = stats[1:, -1]
     nb_components = nb_components - 1
-    #result images
+    # result images
     unique_vehicles = np.zeros((output.shape))
     clustered_vehicles = np.zeros((output.shape))
     for component_id in range(nb_components):
@@ -48,14 +48,14 @@ def is_connected_to(image, label_of_interest, other_label):
     merged_labels[image == label_of_interest] = 255
     merged_labels[image == other_label] = 255
     nb_components = cv.connectedComponents(merged_labels, connectivity=8)[0]
-    #One component for the background + one for the connected labels
+    # One component for the background + one for the connected labels
     return nb_components == 2
 
 
 def connected_labels(image, label_of_interest):
     connected_labels = []
     for current_label in range(1, np.max(image) + 1):
-        if(current_label != label_of_interest and is_connected_to(image, label_of_interest, current_label)):
+        if (current_label != label_of_interest and is_connected_to(image, label_of_interest, current_label)):
             connected_labels.append(current_label)
     return connected_labels
 
@@ -64,17 +64,17 @@ def reassignate_cluster_number(image, first_cluster_number=1):
     new_label_count = first_cluster_number
     new_labels = np.uint8(np.zeros(image.shape))
     for current_label in range(1, np.max(image) + 1):
-        if(np.count_nonzero(image == current_label) > 0):
+        if (np.count_nonzero(image == current_label) > 0):
             new_labels[image == current_label] = new_label_count
             new_label_count += 1
     return new_labels
 
 
-def merge_labels(image, min_size = 80):
+def merge_labels(image, min_size=80):
     number_of_labels_before_merging = len(np.unique(image))
     for current_label in range(np.max(image) + 1):
         cluster_size = np.count_nonzero(image == current_label)
-        if(cluster_size > 0 and cluster_size < min_size):
+        if (cluster_size > 0 and cluster_size < min_size):
             label_connected = connected_labels(image, current_label)
             if (label_connected != []):
                 label_sizes = [np.count_nonzero(image == possible_label) for possible_label in label_connected]
@@ -91,34 +91,34 @@ def merge_labels(image, min_size = 80):
 def extract_single_vehicles(base_img, unique_vehicles):
     markers = ndi.label(unique_vehicles)[0]
     nb_vehicles = len(np.unique(markers)) - 1
-    img = mark_boundaries(base_img, markers, color=[0,255,0], mode="outer")
+    img = mark_boundaries(base_img, markers, color=[0.0, 1.0, 0.0], mode="outer")
     return img, markers, nb_vehicles
 
 
 def extract_clustered_vehicles(base_img, clustered_vehicles, single_vehicle_mean_size):
     distance = ndi.distance_transform_edt(clustered_vehicles)
-    local_maxi = peak_local_max(distance, indices=False, footprint=np.ones((3, 3)), \
-                                labels=clustered_vehicles,                          \
+    local_maxi = peak_local_max(distance, indices=False, footprint=np.ones((3, 3)),
+                                labels=clustered_vehicles,
                                 num_peaks_per_label=single_vehicle_mean_size)
     markers = ndi.label(local_maxi)[0]
     labels = watershed(-distance, markers, mask=clustered_vehicles)
     watershed_markers = merge_labels(labels.copy())
-        
+
     nb_vehicles = len(np.unique(watershed_markers)) - 1
-    img = mark_boundaries(base_img, watershed_markers, color=[255,0,0], mode="outer")
+    img = mark_boundaries(base_img, watershed_markers, color=[1.0, 0.0, 0.0], mode="outer")
     return img, watershed_markers, nb_vehicles
 
 
 def merging_single_and_clustered_results(base_img, unique_markers, clustered_markers, nb_single_vehicles):
     new_clustered_marker = reassignate_cluster_number(clustered_markers, nb_single_vehicles + 1)
     merged_markers = new_clustered_marker + unique_markers
-    img = mark_boundaries(base_img, merged_markers, color=[255,0,0], mode="outer")
+    img = mark_boundaries(base_img, merged_markers, color=[1.0, 0.0, 0.0], mode="outer")
     nb_vehicles = len(np.unique(merged_markers)) - 1
     return img, merged_markers, nb_vehicles
 
 
 def main():
-    TERMINAL_MODE = True #if launched from an IDE, change TERMINAL MODE to False
+    TERMINAL_MODE = True  # if launched from an IDE, change TERMINAL MODE to False
     if (TERMINAL_MODE and len(sys.argv) < 3):
         print("USAGE:\n")
         print("python3 separate_vehicles.py [PREDICTION PATH] [IMAGE TO DISPLAY THE SEGMENTATION ON PATH]")
@@ -130,14 +130,21 @@ def main():
         else:
             img = plt.imread("prediction_cars.tif")
             background_image = plt.imread("DG_2016_02_20.tif")
+
         gray = rgb2gray(img)
         denoised_image = noise_removal(gray)
-        unique_vehicles, clustered_vehicles, single_vehicle_mean_size = separate_vehicles_by_size(denoised_image, separation_size = 160)
+        unique_vehicles, clustered_vehicles, single_vehicle_mean_size = separate_vehicles_by_size(denoised_image,
+                                                                                                  separation_size=160)
         unique_vehicles_results, unique_markers, nb_single_vehicles = extract_single_vehicles(img, unique_vehicles)
-        clustered_vehicles_results, clustered_markers, nb_clustered_vehicles = extract_clustered_vehicles(img, clustered_vehicles, single_vehicle_mean_size)
-        segmented_vehicles_results, markers, nb_vehicles = merging_single_and_clustered_results(background_image, unique_markers, clustered_markers, nb_single_vehicles)
-        print("number of detected vehicles "+ str(nb_vehicles))
-        plt.figure(figsize=(20,20))
+        clustered_vehicles_results, clustered_markers, nb_clustered_vehicles = extract_clustered_vehicles(img,
+                                                                                                          clustered_vehicles,
+                                                                                                          single_vehicle_mean_size)
+        segmented_vehicles_results, markers, nb_vehicles = merging_single_and_clustered_results(background_image,
+                                                                                                unique_markers,
+                                                                                                clustered_markers,
+                                                                                                nb_single_vehicles)
+        print("number of detected vehicles " + str(nb_vehicles))
+        plt.figure(figsize=(20, 20))
         plt.imshow(segmented_vehicles_results)
         plt.imsave("segmented_image.png", segmented_vehicles_results)
 
